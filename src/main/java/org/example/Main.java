047567db23445069d6e08f1cf87d444ac3e991f0
GustavoLyra23 <gustavolyra23@gmail.com>
2024-10-21 21:46:48 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Map<String, String> cache = new HashMap<>();
+    private static String originServer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        var command = sc.nextLine();
+
+
+    }
+}
