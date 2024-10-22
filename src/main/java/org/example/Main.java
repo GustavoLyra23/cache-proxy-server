f8138d4117d69b11e04a0bce1d0ec5396fe30a2c
GustavoLyra23 <gustavolyra23@gmail.com>
2024-10-22 02:03:24 -0300

    refactor main function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,43 @@
 package org.example;
 
+import com.sun.net.httpserver.HttpServer;
 import org.example.service.WebService;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    private static Map<String, String> cache = new HashMap<>();
-    private static String originServer;
-
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         WebService webService = WebService.getInstance();
-        var command = sc.nextLine();
         String regex = "--port\\s+(\\d+)\\s+--origin\\s+(\\S+)";
+
+        do {
+            var command = sc.nextLine();
+            checkCommand(command, regex, webService);
+        } while (true);
+    }
+
+    private static void checkCommand(String command, String regex, WebService webService) throws IOException {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(command);
-        int port;
-
         if (matcher.find()) {
-            port = Integer.parseInt(matcher.group(1));
+            var port = Integer.parseInt(matcher.group(1));
             String origin = matcher.group(2);
             System.out.println("Port: " + port);
             System.out.println("Origin: " + origin);
-
             webService.setOriginServer(origin);
-
             webService.startServer(port);
+        } else if (command.equalsIgnoreCase("exit")) {
+            System.exit(0);
         } else {
-            System.out.println("Invalid input. Please use the format: --port <number> --origin <url>");
+            System.out.println("Invalid command");
         }
     }
+
+
+
 }
